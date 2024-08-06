3453ead4b129b2f41a1c219f1d4946a0accdbf1a
Malva <malvaorbitadigital@gmail.com>
2024-08-06 09:33:33 +0200

    selector con categorías y textarea

--- od_description_categories.php
+++ od_description_categories.php
@@ -42,3 +42,102 @@ class Od_description_categories extends Module
          && parent::uninstall();
    }
 
+   protected function getFormValues()
+   {
+      $values = [];
+      $languages = Language::getLanguages(false);
+      foreach ($languages as $lang) {
+         $values['description2'][$lang['id_lang']] = Tools::getValue('description2_' . $lang['id_lang'], '');
+      }
+
+      $values['category'] = Tools::getValue('category', '');
+      return $values;
+   }
+
+   /**
+    * Handles the module's configuration page
+    * @return string The page's HTML content 
+    */
+   public function getContent()
+   {
+      $output = '';
+
+      return $output . $this->displayForm();
+   }
+
+   private function getCategories()
+   {
+      $categories = Category::getCategories();
+      $options = [];
+      foreach ($categories as $category) {
+         foreach ($category as $info) {
+            if ($info['infos']['level_depth'] < 2) continue;
+
+            $options[] = [
+               'id' => $info['infos']['id_category'],
+               'name' => $info['infos']['name']
+            ];
+         }
+      }
+      return $options;
+   }
+
+   /**
+    * Builds the configuration form
+    * @return string HTML code
+    */
+   public function displayForm()
+   {
+      $form = [
+         'form' => [
+            'tinymce' => true,
+            'legend' => [
+               'title' => $this->l('Descriptions'),
+            ],
+            'input' => [
+               [
+                  'type' => 'select',
+                  'label' => $this->l('Category'),
+                  'name' => 'category',
+                  'required' => true,
+                  'options' => [
+                     'query' => $this->getCategories(),
+                     'id' => 'id',
+                     'name' => 'name',
+                  ]
+               ], [
+                  'type' => 'textarea',
+                  'name' => 'description2',
+                  'label' => $this->l('Description 2'),
+                  'required' => true,
+                  'lang' => true,
+                  'rows' => 3,
+                  'class' => 'rte',
+                  'autoload_rte' => true,
+               ]
+            ],
+            'submit' => [
+               'title' => $this->l('Save'),
+               'class' => 'btn btn-default pull-right'
+            ]
+         ],
+      ];
+
+      $helper = new HelperForm();
+
+      $helper->module = $this;
+      $helper->identifier = $this->identifier;
+      $helper->token = Tools::getAdminTokenLite('AdminModules');
+      $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
+      $helper->default_form_language = $this->context->language->id;
+      $helper->tpl_vars = [
+         'fields_value' => $this->getFormValues(),
+         'languages'    => $this->context->controller->getLanguages(),
+         'id_language'  => $this->context->language->id,
+      ];
+
+      $helper->submit_action = 'submit' . $this->name;
+
+      return $helper->generateForm([$form]);
+   }
+}
